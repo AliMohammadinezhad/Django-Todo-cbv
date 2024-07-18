912a9a43880d2138a706d74b6b32d4964068c516
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-07-18 16:53:01 +0330

    add and complete todo app

---
+++ core/todo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'todo'
+
+urlpatterns = [
+    path("", views.TodoListView.as_view(), name="todo-list"),
+    path("create/", views.TodoCreateView.as_view(), name="todo-create"),
+    path("update/<int:pk>/", views.TodoUpdateView.as_view(), name="todo-update"),
+    path("complete/<int:pk>/", views.TodoCompleteView.as_view(), name="todo-complete"),
+    path("delete/<int:pk>/", views.TodoDeleteView.as_view(), name="todo-delete"),
+    
+]
+
---
+++ core/todo/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+
+from .models import Todo
+from .forms import TodoForm
+
+
+class TodoListView(LoginRequiredMixin, ListView):
+    model = Todo
+    template_name = "index.html"
+    context_object_name = "todos"
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class TodoCreateView(LoginRequiredMixin, CreateView):
+    model = Todo
+    fields = ["name"]
+    template_name = "create.html"
+    context_object_name = "todo"
+    success_url = reverse_lazy("todo:todo-list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TodoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Todo
+    fields = ["status"]
+    success_url = reverse_lazy("todo:todo-list")
+    template_name = "index.html"
+
+
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Todo
+    context_object_name = "todo"
+    success_url = reverse_lazy("todo:todo-list")
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return redirect(self.get_success_url())
+    
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class TodoCompleteView(LoginRequiredMixin, View):
+    model = Todo
+    success_url = reverse_lazy("todo:todo-list")
+    
+
+    def get(self, request, *args, **kwargs):
+        object = Todo.objects.get(id=kwargs.get("pk"))
+        object.status = True
+        object.save()
+        return redirect(self.success_url)

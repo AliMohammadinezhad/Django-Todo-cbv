4ac284c1971625d7fc70fb238a46ae474cd83fad
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-07-20 23:19:26 +0330

    add accounts app for authentication

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("todo/", include("todo.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("", include("todo.urls")),
     ]
 
 if settings.DEBUG:
